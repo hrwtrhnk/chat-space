74710ff82e67aa3dd6c218d28839639f447af8e6
わたり。 <yue.hinako.0175@gmail.com>
2019-07-17 12:40:54 +0900

    message done!!!!!!!!!

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_latest_message
+    if (latest_message = messages.last).present?
+      latest_message.content? ? latest_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
