7b5cba1a1b38cb504aea9b86253ef20008827b28
わたり。 <yue.hinako.0175@gmail.com>
2019-07-18 19:23:39 +0900

    incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?) and id !=(?)', "%#{params[:keyword]}%", current_user.id)
+    respond_to do |format|
+      format.json
+      format.html
+    end
+  end
+
   def edit
   end
 
