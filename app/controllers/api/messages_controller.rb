7f078e2a4c9bd727e7f9b786917959b1f408f87b
わたり。 <yue.hinako.0175@gmail.com>
2019-07-20 12:32:38 +0900

    deleted commentouts

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,11 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # @group = Group.find(params[:group_id])
-    # @message = Message.new
     @messages = Message.includes(:user).where('id > ?', params[:id])
-    # respond_to do |format|
-    #   format.json { @new_message = @messages.where('id > ?', params[:id]) }
-    #   format.html
-    # end
   end
 end
